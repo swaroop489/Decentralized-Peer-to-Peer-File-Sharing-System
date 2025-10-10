5add1325d7464cc393cb3045d7550767f77cbafa
Swaroop Sandanshive <swaroopsandanshive@gmail.com>
2025-10-10 13:10:23 +0530

    added single-page Home UI with Navbar, file upload, and peers section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+// import Files from "./pages/Files";
+// import Login from "./pages/Login";
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/files" element={<Files />} />
+        <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
